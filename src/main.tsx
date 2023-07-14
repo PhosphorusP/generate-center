98ae3691e2ff388c9499073228afe4eb2b0d928b
PhosphorusP <haskiiiii@qq.com>
2023-07-15 00:55:42 +0800

    refactor: rename App.tsx
    Rename App.tsx to AppWrapper.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { AppWrapper } from './App.tsx';
+import { AppWrapper } from './AppWrapper.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
