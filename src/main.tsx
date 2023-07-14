e7f3983d12ff460c550b64da4f8a28816b9b2c15
PhosphorusP <haskiiiii@qq.com>
2023-07-15 01:01:54 +0800

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 5a0e0d1205c07731dc9724f07f58407ebeffd56f, reversing
    changes made to 98ae3691e2ff388c9499073228afe4eb2b0d928b.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import { AppWrapper } from './AppWrapper.tsx';
-=======
-import { AppWrapper } from './App.tsx';
->>>>>>> origin/main
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
